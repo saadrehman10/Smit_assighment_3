4137600d202e77de23c0daf2547f2e60ac6b5ba8
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-29 22:40:30 +0500

    update

--- new.dart
+++ new.dart
@@ -1,1 +1,12 @@
-void main {} 
+import 'dart:io';
+
+void main() {
+  for (int i = 0; i < 25; i++) {
+    File file = File('./task${i + 1}.dart');
+    file.writeAsStringSync('''
+    void main(){
+     print('');
+    }
+''');
+  }
+}
