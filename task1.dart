5ffb2cfa1f195bee415404f9c7b5d4849fb9b108
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-01 15:29:54 +0500

    update

--- task1.dart
+++ task1.dart
@@ -1,3 +1,17 @@
-    void main(){
-     print('');
-    }
+import 'dart:math';
+
+void main() {
+  print('''
+        Write a program that takes a list of numbers as input and prints the 
+        even numbers in the list using a for loop.
+        Example:
+        Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        Output: 2 4 6 8 10
+          ''');
+
+  List<int> numbers = List<int>.generate(20, (index) => Random().nextInt(100));
+
+  for (int i = 0; i < numbers.length; i++) {
+    numbers[i] % 2 == 0 ? print(i) : null;
+  }
+}
--- task2.dart
+++ task2.dart
@@ -1,3 +1,16 @@
-    void main(){
-     print('');
+void main() {
+  print('''Write a program that prints the Fibonacci sequence up to a given 
+          number using a for loop.
+          Example:
+          Input: 10
+          Output: 0 1 1 2 3 5 8''');
+
+  int number = 10;
+  for (int i = 0; i <= number; i++) {
+    if (i >= 1) {
+      print((i - 1).abs() + (i - 2).abs());
+    } else {
+      print(0);
     }
+  }
+}
