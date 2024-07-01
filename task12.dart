6a4d773670239945bc50d953fadd1d82decf3052
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-01 19:37:08 +0500

    update

--- task12.dart
+++ task12.dart
@@ -1,3 +1,15 @@
-    void main(){
-     print('');
-    }
+void main() {
+  print('''
+    Write a program to display a pattern like a right angle triangle with a 
+    number using loop.
+    The pattern like :
+    1
+    12
+    123
+    1234
+    ''');
+  String temp = '';
+  for (int i = 1; i < 5; i++) {
+    print(temp += i.toString());
+  }
+}
