9840ca56e7d23f855fe995aab6f127fab8fc671b
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-01 19:31:05 +0500

    update

--- task11.dart
+++ task11.dart
@@ -1,3 +1,16 @@
-    void main(){
-     print('');
-    }
+void main() {
+  print('''
+    Write a program to display a pattern like a right angle triangle using an 
+    asterisk using loop.
+    The pattern like :
+    *
+    **
+    ***
+    ****
+
+    ''');
+
+  for (int i = 0; i < 5; i++) {
+    print('*' * i);
+  }
+}
