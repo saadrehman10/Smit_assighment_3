5d324b1e9817ff9a144466059862acc3ec554cbb
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-03 02:19:51 +0500

    update

--- task24.dart
+++ task24.dart
@@ -1,3 +1,22 @@
-    void main(){
-     print('');
+void main() {
+  print(
+      'Implement a code that finds the average of all the negative numbers in a list using a for loop and if-else condition.');
+
+  List<int> numbers = [10, -5, 8, -3, -12, 7, -15];
+  int sumOfNegatives = 0;
+  int countNegatives = 0;
+
+  for (int number in numbers) {
+    if (number < 0) {
+      sumOfNegatives += number;
+      countNegatives++;
     }
+  }
+
+  if (countNegatives > 0) {
+    double averageNegative = sumOfNegatives / countNegatives;
+    print('Average of negative numbers: ${averageNegative.toStringAsFixed(2)}');
+  } else {
+    print('No negative numbers found in the list.');
+  }
+}
--- task25.dart
+++ task25.dart
@@ -1,3 +1,37 @@
-    void main(){
-     print('');
+import 'dart:math';
+
+void main() {
+  print('''
+    Write a program that takes a list of integers as input and returns a new 
+    list containing only the prime numbers from the original list. Implement 
+    the solution using a for loop and logical operations.
+    Input: [4, 7, 10, 13, 16, 19, 22, 25, 28, 31]
+    Output:  [7, 13, 19, 31]
+    ''');
+
+  List<int> inputList = [4, 7, 10, 13, 16, 19, 22, 25, 28, 31];
+  List<int> primeNumbers = [];
+
+  bool isPrime(int num) {
+    if (num <= 1) {
+      return false;
     }
+
+    for (int i = 2; i * i <= num; i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  for (int number in inputList) {
+    if (isPrime(number)) {
+      primeNumbers.add(number);
+    }
+  }
+
+  print('Input list: $inputList');
+  print('Prime numbers: $primeNumbers');
+}
