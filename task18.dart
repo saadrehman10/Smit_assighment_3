f47db1348125faf8d133e2ddf39af5673fb12423
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-03 01:18:37 +0500

    update

--- task18.dart
+++ task18.dart
@@ -1,3 +1,10 @@
-    void main(){
-     print('');
-    }
+void main() {
+  print('''
+    Write a program that asks the user for their email and password. You 
+    are given a list of predefined user credentials (email and password 
+    combinations). If the entered email and password match any of the 
+    credentials in the list, print "User login successful." Otherwise, keep 
+    asking for the email and password until the correct credentials are 
+    provided
+    ''');
+}
