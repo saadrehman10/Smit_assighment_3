d2d14d73b5e34030a04bfacf282c71104035cca5
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-03 02:13:36 +0500

    update

--- task22.dart
+++ task22.dart
@@ -1,3 +1,14 @@
 void main() {
-  print('');
+  print(
+      'Write a program that calculates the sum of the squares of all odd numbers in a given list using a for loop and if-else condition');
+  List<int> numbers = List<int>.generate(20, (index) => index);
+  int sumOfOddSquares = 0;
+
+  for (int number in numbers) {
+    if (number % 2 == 0) {
+      sumOfOddSquares += number * number;
+    }
+  }
+
+  print('Sum of squares of odd numbers: $sumOfOddSquares');
 }
--- task23.dart
+++ task23.dart
@@ -1,3 +1,58 @@
-    void main(){
-     print('');
+void main() {
+  print('''
+    Write a program that takes a list of student details as input, where 
+    each student is represented by a map containing their name, marks, 
+    section, and roll number. The program should determine the grade of 
+    each student based on their average score (assuming maximum marks 
+    for each subject is 100) and print the student's name along with their 
+    grade.
+    List<Map<String, dynamic>> studentDetails = [
+      {'name': 'John', 'marks': [80, 75, 90], 'section': 'A', 'rollNumber': 101},
+      {'name': 'Emma', 'marks': [95, 92, 88], 'section': 'B', 'rollNumber': 102},
+      {'name': 'Ryan', 'marks': [70, 65, 75], 'section': 'A', 'rollNumber': 103},
+    ];
+    ''');
+
+  List<Map<String, dynamic>> studentDetails = [
+    {
+      'name': 'John',
+      'marks': [80, 75, 90],
+      'section': 'A',
+      'rollNumber': 101
+    },
+    {
+      'name': 'Emma',
+      'marks': [95, 92, 88],
+      'section': 'B',
+      'rollNumber': 102
+    },
+    {
+      'name': 'Ryan',
+      'marks': [70, 65, 75],
+      'section': 'A',
+      'rollNumber': 103
+    },
+  ];
+
+  for (var student in studentDetails) {
+    String name = student['name'];
+    List<int> marks = student['marks'];
+
+    double average = marks.reduce((a, b) => a + b) / marks.length;
+    String grade;
+
+    if (average >= 90) {
+      grade = 'A';
+    } else if (average >= 80) {
+      grade = 'B';
+    } else if (average >= 70) {
+      grade = 'C';
+    } else if (average >= 60) {
+      grade = 'D';
+    } else {
+      grade = 'F';
     }
+
+    print('Student: $name, Grade: $grade');
+  }
+}
