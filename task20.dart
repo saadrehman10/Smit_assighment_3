940f2b5595437c1e007fd10ecfa9773a4ed119b7
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-03 02:00:03 +0500

    update

--- task20.dart
+++ task20.dart
@@ -1,3 +1,14 @@
-    void main(){
-     print('');
+void main() {
+  print(
+      'Write a program that counts the number of vowels in a given string using a for loop and if-else condition.');
+
+  String str = 'Random String';
+  List<String> vowel = 'aeiou'.split('');
+  int count = 0;
+  for (int i = 0; i < str.length; i++) {
+    if (vowel.contains(str[i])) {
+      count++;
     }
+  }
+  print('The String "$str" contains $count vowels');
+}
--- task21.dart
+++ task21.dart
@@ -1,3 +1,21 @@
-    void main(){
-     print('');
+import 'dart:math';
+
+void main() {
+  print(
+      'Implement a code that finds the maximum and minimum elements in a list using a for loop and if-else condition');
+
+  List<int> numbers = List<int>.generate(40, (index) => Random().nextInt(100));
+  int max = numbers[0];
+  int min = numbers[0];
+
+  for (int i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    } else if (numbers[i] < min) {
+      min = numbers[i];
     }
+  }
+  print(numbers);
+  print('Maximum element: $max');
+  print('Minimum element: $min');
+}
