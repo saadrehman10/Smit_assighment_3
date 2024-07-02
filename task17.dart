84b763f544e7687b0dea8e7fdd529aa6c06f8247
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-03 01:16:14 +0500

    update

--- task17.dart
+++ task17.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
   print('''
       Write a program that asks the user for their email and password. If the 
@@ -6,5 +8,17 @@ void main() {
       until the correct credentials are provided.
       ''');
 
-      
+  String username = 'abc@abc.com', password = '123';
+  while (true) {
+    stdout.write('Enter Username: ');
+    String usernamecheck = stdin.readLineSync()!;
+    stdout.write('Enter Password: ');
+    String passwordcheck = stdin.readLineSync()!;
+    if (usernamecheck == username && passwordcheck == password) {
+      print('\n\tLogin Successful');
+      break;
+    } else {
+      print('\n\tWrong Credential');
+    }
+  }
 }
