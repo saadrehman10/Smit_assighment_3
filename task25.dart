cccdbae615393f1a0fae668985cf6fe9d92b656f
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-03 02:21:37 +0500

    update

--- task25.dart
+++ task25.dart
@@ -12,22 +12,21 @@ void main() {
   List<int> inputList = [4, 7, 10, 13, 16, 19, 22, 25, 28, 31];
   List<int> primeNumbers = [];
 
-  bool isPrime(int num) {
-    if (num <= 1) {
-      return false;
-    }
+  for (int number in inputList) {
+    bool isPrime = true;
 
-    for (int i = 2; i * i <= num; i++) {
-      if (num % i == 0) {
-        return false;
+    if (number <= 1) {
+      isPrime = false;
+    } else {
+      for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+          isPrime = false;
+          break;
+        }
       }
     }
 
-    return true;
-  }
-
-  for (int number in inputList) {
-    if (isPrime(number)) {
+    if (isPrime) {
       primeNumbers.add(number);
     }
   }
