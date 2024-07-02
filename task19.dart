6c0bef942f053aac9f7613d88293130254f4ffaf
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-03 01:49:10 +0500

    update

--- task19.dart
+++ task19.dart
@@ -1,3 +1,12 @@
-    void main(){
-     print('');
-    }
+import 'dart:math';
+
+void main() {
+  print(
+      'Write a program that takes a list of numbers as input and prints the numbers greater than 5 using a for loop and if-else condition.');
+
+  List<int> numbers = List<int>.generate(20, (index) => Random().nextInt(13));
+  print(numbers);
+  for (int i = 0; i < numbers.length; i++) {
+    numbers[i] > 5 ? print(numbers[i]) : null;
+  }
+}
