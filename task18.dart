977ba9e1adadbdbaf006eab305fed451b2c4b0d6
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-03 01:45:13 +0500

    update

--- task18.dart
+++ task18.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
   print('''
     Write a program that asks the user for their email and password. You 
@@ -7,4 +9,46 @@ void main() {
     asking for the email and password until the correct credentials are 
     provided
     ''');
+
+  List<String> usernames = [
+    'abc@abc.com',
+    'CoolCoder123',
+    'AdventureSeeker',
+    'PixelMaster',
+    'CoffeeLover',
+    'GuitarHero42',
+    'TechExplorer',
+    'NatureEnthusiast',
+    'StarGazer99',
+    'Bookworm2023',
+    'MusicMaker',
+  ];
+
+  List<String> passwords = [
+    '123',
+    'P@ssw0rd123',
+    'SecurePass!',
+    'SecretCode42',
+    'Rainbow2023',
+    'NinjaWarrior',
+    '1234LetMeIn',
+    'BlueSkyunshine',
+    'Coffee&C0de',
+    'MoonlightDreams',
+    'FluffyKittens#',
+  ];
+
+  while (true) {
+    stdout.write('Enter Username: ');
+    String usernamecheck = stdin.readLineSync()!;
+    stdout.write('Enter Password: ');
+    String passwordcheck = stdin.readLineSync()!;
+    if (usernames.contains(usernamecheck) &&
+        passwords.contains(passwordcheck)) {
+      print('\n\tLogin Successful');
+      break;
+    } else {
+      print('\n\tWrong Credential');
+    }
+  }
 }
