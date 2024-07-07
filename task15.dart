6ca489b0268ffc03d5cd083fa015efd16fccdc90
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-07 21:21:50 +0500

    update

--- task15.dart
+++ task15.dart
@@ -12,16 +12,14 @@ void main() {
       ''');
 
   int row = 4;
-  int num = 1; // Initialize the starting number
+  int num = 1;
   for (int i = 0; i < row; i++) {
-    // Print spaces before the numbers
     for (int j = 0; j < row - i; j++) {
       stdout.write(' ');
     }
-    // Print the numbers in the pyramid
     for (int k = 0; k <= i; k++) {
       stdout.write('$num ');
-      num++; // Increment the number for the next iteration
+      num++; 
     }
     print('');
   }
